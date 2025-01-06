eb1b7a12144b31b02472153c9b7f7a65959da7a8
Waiz <164471173+decodewidwaiz@users.noreply.github.com>
2025-01-07 01:14:10 +0530

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,5 @@ import datetime
 today = datetime.date.today()
 print("Today's date is:", today)
 
-###standard date 
 formatted_date = today.strftime("%d-%m-%Y")
 print("Today's date is:", formatted_date)
